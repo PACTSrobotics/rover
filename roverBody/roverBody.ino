eb95455ee047911190a8208fb8013b4d09f3a371
pacts <ibenamos@gmail.com>
2015-11-28 15:52:04 -0500

    started conversion from dalek to rover

--- roverBody/roverBody.ino
+++ roverBody/roverBody.ino
@@ -1,26 +1,44 @@
+#include <H_Bridge.h>
 // Note: Arduino 1.0 and later version IDE has SoftwareSerial Library
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
 SoftwareSerial XBeeSerial(2, 3);
-                
-#define LEFT_PIN           6                   
-#define RIGHT_PIN          11    
+
+
+#define ARM_PIN_HORIZONTAL         11
+
+
+#define LEFT_DRIVE_PIN_1  13
+#define LEFT_DRIVE_PIN_2  12
+#define LEFT_DRIVE_EN_PIN 10
+
+#define RIGHT_DRIVE_PIN_1 7
+#define RIGHT_DRIVE_PIN_2 8
+#define RIGHT_DRIVE_EN_PIN 9
+
+#define ARM_DRIVE_PIN_1 A0
+#define ARM_DRIVE_PIN_2 4
+#define ARM_DRIVE_EN_PIN 6
+
+
+
 // Input Commands#define HEAD_PIN           10
 
-#define BEGIN_COMMAND  0x7F  // decimal = 127, binary = 0111 1111
+#define BEGIN_COMMAND  0x77  // decimal = 119, binary = 0111 0111
 #define MOTORLEFT      0x1   // decimal =   1, binary = 0000 0001
 #define MOTORRIGHT     0x2   // decimal =   2, binary = 0000 0010
-Servo Left;
-Servo Right;
 
+Servo armServo;
+
+H_Bridge leftDrive(LEFT_DRIVE_PIN_1, LEFT_DRIVE_PIN_2, LEFT_DRIVE_EN_PIN);
+H_Bridge rightDrive(RIGHT_DRIVE_PIN_1, RIGHT_DRIVE_PIN_2, RIGHT_DRIVE_EN_PIN);
+H_Bridge armDrive(ARM_DRIVE_PIN_1, ARM_DRIVE_PIN_2,ARM_DRIVE_EN_PIN);
 int command[2];
 
 void setup() {
   XBeeSerial.begin(9600);     
-  Left.attach(LEFT_PIN);
-  Right.attach(RIGHT_PIN);
-  pinMode(13, OUTPUT);
+  armServo.attach(ARM_PIN_HORIZONTAL);
 }
 
 void loop() {
@@ -28,9 +46,7 @@ void loop() {
   if (XBeeSerial.available() > 0) {
     if (readCommand() > 0) {
       executeCommand();
-      digitalWrite(13,HIGH);
     }
-    digitalWrite(13, HIGH);
   }
 }
 
@@ -59,10 +75,13 @@ void executeCommand() {
   int speed = command[1];
   
   if(c == MOTORLEFT){
-    Left.write(speed);
+     if(speed >= 0) {
+       leftDrive.start(leftDrive.FORWARD, map(constrain(speed,0,127), 0, 127, 0, 255));
+     }
+     
   }
   if(c == MOTORRIGHT){
-    Right.write(speed);
+
   }
 
 }
